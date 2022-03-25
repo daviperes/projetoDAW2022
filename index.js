dc55a38712d38690a5016c7dd8b4cff5d702c8cb
Diego de Abreu Porcellis <dapporcellis@gmail.com>
2022-03-25 16:05:30 -0300

    Projeto

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ app.get("/usuario/", function (req, res) {
   });
 });
 
+//rota para listar dados
+app.get("/projeto/", function (req, res) {
+  Projeto.find({}).then(function (docs) {
+    res.render("projeto/list.ejs", { Projetos: docs });
+  });
+});
+
 app.post("/usuario/", function (req, res) {
   if (req.body.tipo == "nome") {
     Usuario.find({ nome: new RegExp(req.body.pesquisa, "i") }).then(function (
@@ -34,6 +41,14 @@ app.post("/usuario/", function (req, res) {
       res.render("usuario/list.ejs", { Usuarios: docs });
     });
   }
+});
+
+app.post("/projeto/", function (req, res) {
+  Projeto.find({ nome: new RegExp(req.body.pesquisa, "i") }).then(function (
+    docs
+  ) {
+    res.render("projeto/list.ejs", { Projetos: docs });
+  });
 });
 
 //rota de abrir tela do add
@@ -53,9 +68,12 @@ app.post("/usuario/add", upload.single("foto"), function (req, res) {
   var usuario = new Usuario({
     nome: req.body.nome,
     email: req.body.email,
+    endereco: req.body.endereco,
     senha: req.body.senha,
-    foto: req.file.filename,
   });
+  if (req.file.filename != "") {
+    usuario.foto = req.file.filename;
+  }
 
   usuario.save(function (err, docs) {
     if (err) {
@@ -73,9 +91,11 @@ app.post("/projeto/add", upload.single("foto"), function (req, res) {
     nome: req.body.nome,
     descricao: req.body.descricao,
     dataInicio: req.body.dataInicio,
-    previsaoTermino: req.body.previsoTermino,
-    foto: req.file.filename,
+    previsaoTermino: req.body.previsaoTermino,
   });
+  if (req.file.filename != "") {
+    usuario.foto = req.file.filename;
+  }
 
   projeto.save(function (err, docs) {
     if (err) {
@@ -91,6 +111,13 @@ app.get("/usuario/edt/:id", function (req, res) {
   Usuario.findById(req.params.id).then(function (docs) {
     console.log(docs);
     res.render("usuario/edit.ejs", { Usuario: docs });
+  });
+});
+
+app.get("/projeto/edt/:id", function (req, res) {
+  Projeto.findById(req.params.id).then(function (docs) {
+    console.log(docs);
+    res.render("projeto/edit.ejs", { Projeto: docs });
   });
 });
 
@@ -113,12 +140,42 @@ app.post("/usuario/edt/:id", upload.single("foto"), function (req, res) {
   );
 });
 
+app.post("/projeto/edt/:id", upload.single("foto"), function (req, res) {
+  Projeto.findByIdAndUpdate(
+    req.params.id,
+    {
+      nome: req.body.nome,
+      descricao: req.body.descricao,
+      dataInicio: req.body.dataInicio,
+      previsaoTermino: req.body.previsaoTermino,
+      foto: req.file.filename,
+    },
+    function (err, docs) {
+      if (err) {
+        res.send("Aconteceu um erro:" + err);
+      } else {
+        res.redirect("/projeto/");
+      }
+    }
+  );
+});
+
 app.get("/usuario/del/:id", function (req, res) {
   Usuario.findByIdAndDelete(req.params.id, function (err, doc) {
     if (err) {
       res.send("Aconteceu o seguinte erro: " + err);
     } else {
       res.redirect("/usuario/");
+    }
+  });
+});
+
+app.get("/projeto/del/:id", function (req, res) {
+  Projeto.findByIdAndDelete(req.params.id, function (err, doc) {
+    if (err) {
+      res.send("Aconteceu o seguinte erro: " + err);
+    } else {
+      res.redirect("/projeto/");
     }
   });
 });
--- model/Usuario.js
+++ model/Usuario.js
@@ -3,6 +3,7 @@ var conexao = require("../config/database");
 var UsuarioSchema = conexao.Schema({
   nome: { type: "String" },
   email: { type: "String" },
+  endereco: { type: "String" },
   foto: { type: "String" },
   senha: { type: "String" },
 });
