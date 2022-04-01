554f4858d6f3abd47a1f77e3ace1b45d88528e98
Diego de Abreu Porcellis <dapporcellis@gmail.com>
2022-04-01 15:31:11 -0300

    criando pasta controller

--- routes/projetoRoute.js
+++ routes/projetoRoute.js
@@ -1,86 +1,20 @@
 const express = require("express");
 const Router = express.Router();
-var Projeto = require("../model/Projeto");
 var upload = require("../config/multer.js");
+const projetoController = require("../controller/projetoController");
 
-//rota para listar dados
-Router.get("/projeto/", function (req, res) {
-  Projeto.find({}).then(function (docs) {
-    res.render("projeto/list.ejs", { Projetos: docs });
-  });
-});
+Router.get("/projeto/", projetoController.listarProjetos);
 
-Router.post("/projeto/", function (req, res) {
-  Projeto.find({ nome: new RegExp(req.body.pesquisa, "i") }).then(function (
-    docs
-  ) {
-    res.render("projeto/list.ejs", { Projetos: docs });
-  });
-});
+Router.post("/projeto/", projetoController.listarFiltro);
 
-//rota de abrir tela do add
-Router.get("/projeto/add", function (req, res) {
-  res.render("projeto/add.ejs", {});
-});
-//fim abrir tela de add
+Router.get("/projeto/add", projetoController.abreAdd);
 
-//adicionar dados no banco
-Router.post("/projeto/add", upload.single("foto"), function (req, res) {
-  var projeto = new Projeto({
-    nome: req.body.nome,
-    descricao: req.body.descricao,
-    dataInicio: req.body.dataInicio,
-    previsaoTermino: req.body.previsaoTermino,
-  });
-  if (req.file.filename != "") {
-    usuario.foto = req.file.filename;
-  }
+Router.post("/projeto/add", upload.single("foto"), projetoController.add);
 
-  projeto.save(function (err, docs) {
-    if (err) {
-      res.send("Aconteceu o seguinte erro: " + err);
-    } else {
-      res.redirect("/projeto/");
-    }
-  });
-});
-//fim adicionar dados no banco
+Router.get("/projeto/edt/:id", projetoController.abreEdit);
 
-Router.get("/projeto/edt/:id", function (req, res) {
-  Projeto.findById(req.params.id).then(function (docs) {
-    console.log(docs);
-    res.render("projeto/edit.ejs", { Projeto: docs });
-  });
-});
+Router.post("/projeto/edt/:id", upload.single("foto"), projetoController.edit);
 
-Router.post("/projeto/edt/:id", upload.single("foto"), function (req, res) {
-  Projeto.findByIdAndUpdate(
-    req.params.id,
-    {
-      nome: req.body.nome,
-      descricao: req.body.descricao,
-      dataInicio: req.body.dataInicio,
-      previsaoTermino: req.body.previsaoTermino,
-      foto: req.file.filename,
-    },
-    function (err, docs) {
-      if (err) {
-        res.send("Aconteceu um erro:" + err);
-      } else {
-        res.redirect("/projeto/");
-      }
-    }
-  );
-});
-
-Router.get("/projeto/del/:id", function (req, res) {
-  Projeto.findByIdAndDelete(req.params.id, function (err, doc) {
-    if (err) {
-      res.send("Aconteceu o seguinte erro: " + err);
-    } else {
-      res.redirect("/projeto/");
-    }
-  });
-});
+Router.get("/projeto/del/:id", projetoController.deletar);
 
 module.exports = Router;
--- routes/usuarioRoute.js
+++ routes/usuarioRoute.js
@@ -1,92 +1,20 @@
 const express = require("express");
 const Router = express.Router();
-var Usuario = require("../model/Usuario");
 var upload = require("../config/multer.js");
+var usuarioController = require("../controller/usuarioController");
 
-//rota para listar dados
-Router.get("/usuario/", function (req, res) {
-  Usuario.find({}).then(function (docs) {
-    res.render("usuario/list.ejs", { Usuarios: docs });
-  });
-});
+Router.get("/usuario/", usuarioController.listaUsuarios);
 
-Router.post("/usuario/", function (req, res) {
-  if (req.body.tipo == "nome") {
-    Usuario.find({ nome: new RegExp(req.body.pesquisa, "i") }).then(function (
-      docs
-    ) {
-      res.render("usuario/list.ejs", { Usuarios: docs });
-    });
-  } else {
-    Usuario.find({ email: new RegExp(req.body.pesquisa, "i") }).then(function (
-      docs
-    ) {
-      res.render("usuario/list.ejs", { Usuarios: docs });
-    });
-  }
-});
+Router.post("/usuario/", usuarioController.listarFiltro);
 
-//rota de abrir tela do add
-Router.get("/usuario/add", function (req, res) {
-  res.render("usuario/add.ejs", {});
-});
+Router.get("/usuario/add", usuarioController.abreAdd);
 
-//adicionar dados no banco
-Router.post("/usuario/add", upload.single("foto"), function (req, res) {
-  var usuario = new Usuario({
-    nome: req.body.nome,
-    email: req.body.email,
-    endereco: req.body.endereco,
-    senha: req.body.senha,
-  });
-  if (req.file.filename != "") {
-    usuario.foto = req.file.filename;
-  }
+Router.post("/usuario/add", upload.single("foto"), usuarioController.add);
 
-  usuario.save(function (err, docs) {
-    if (err) {
-      res.send("Aconteceu o seguinte erro: " + err);
-    } else {
-      res.redirect("/usuario/");
-    }
-  });
-});
-//fim adicionar dados no banco
+Router.get("/usuario/edt/:id", usuarioController.abreEdit);
 
-Router.get("/usuario/edt/:id", function (req, res) {
-  Usuario.findById(req.params.id).then(function (docs) {
-    console.log(docs);
-    res.render("usuario/edit.ejs", { Usuario: docs });
-  });
-});
+Router.post("/usuario/edt/:id", upload.single("foto"), usuarioController.edita);
 
-Router.post("/usuario/edt/:id", upload.single("foto"), function (req, res) {
-  Usuario.findByIdAndUpdate(
-    req.params.id,
-    {
-      nome: req.body.nome,
-      email: req.body.email,
-      senha: req.body.senha,
-      foto: req.file.filename,
-    },
-    function (err, docs) {
-      if (err) {
-        res.send("Aconteceu um erro:" + err);
-      } else {
-        res.redirect("/usuario/");
-      }
-    }
-  );
-});
-
-Router.get("/usuario/del/:id", function (req, res) {
-  Usuario.findByIdAndDelete(req.params.id, function (err, doc) {
-    if (err) {
-      res.send("Aconteceu o seguinte erro: " + err);
-    } else {
-      res.redirect("/usuario/");
-    }
-  });
-});
+Router.get("/usuario/del/:id", usuarioController.deleta);
 
 module.exports = Router;
